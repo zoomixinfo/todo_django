469030682c28c51318b13f42b7f572054e40163f
Sameer Kongath <info@zoomixinfo.in>
2021-06-09 09:16:10 +0530

    Todo Application in Django

---
+++ todo/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.shortcuts import render
+
+class Todo(models.Model):
+    title=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.title
---
+++ todo/views.py
@@ -0,0 +1,18 @@
+import todo
+from django.shortcuts import redirect, render
+from .models import Todo
+
+def index(request):
+    todo = Todo.objects.all()
+    if request.method == 'POST':
+        new_todo = Todo(
+            title = request.POST['title']
+        )
+        new_todo.save()
+        return redirect('/')
+    return render(request, 'index.html',{'todos':todo})
+def delete(request, pk):
+    todo = Todo.objects.get(id=pk)
+    todo.delete()
+    return redirect('/')
+
